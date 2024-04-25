93cb97ce349100e1b3831ffe1f956858d38cc6af
atulya1028 <atulyaagrawal1996@gmail.com>
2024-04-25 18:42:48 +0530

    Screens Changes

--- src/pages/AccountSetting.jsx
+++ src/pages/AccountSetting.jsx
@@ -4,14 +4,16 @@ import profile from "../images/profile.png";
 import camera from "../images/camera.png";
 
 export const AccountSetting = () => {
+
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   const handleScreenWidth = () => {
     setScreenWidth(window.innerWidth);
   };
 
+
   useEffect(() => {
-    console.log("Width:", window.innerWidth);
+    console.log('Width:',window.innerWidth);
     handleScreenWidth();
     window.addEventListener("resize", handleScreenWidth);
 
@@ -20,35 +22,37 @@ export const AccountSetting = () => {
     };
   }, []);
 
+
   console.log("Width: ", window.innerWidth);
+
+  let minimize = "mobile";
+  if (screenWidth >= 320 && screenWidth <= 480) {
+    minimize = "tablet";
+  } else if (screenWidth >= 481 && screenWidth <= 768) {
+    minimize = "laptops";
+  } else if (screenWidth >= 769 && screenWidth <= 1024) {
+    minimize = "desktop";
+  } else if (screenWidth >= 1025 && screenWidth <= 1200) {
+    minimize = "extra";
+  } else if(screenWidth>=1201) {
+    minimize ="extra-large";
+  }
+
+  let maximize = "over";
+  if(screenWidth>=1201){
+    maximize='xxl';
+  }
 
   return (
     <>
       <div className="header">Account Settings</div>
-      <div className="block" style={{}}>
+      <div className="block" style={{
+      }}>
         <div className="profile-main">
-          <div
-            className="profile"
-          >
-            <div className="intro">
+          <div className="profile">
+            <div className="intro" >
               <img src={profile} alt="Profile" />
-              <img
-                src={camera}
-                alt="Camera"
-                className={
-                  screenWidth >= 320 && screenWidth <= 480
-                    ? "mobile"
-                    : screenWidth >= 481 && screenWidth <= 768
-                    ? "tablet"
-                    : screenWidth >= 769 && screenWidth <= 1024
-                    ? "laptops"
-                    : screenWidth >= 1025 && screenWidth <= 1200
-                    ? "desktop"
-                    : screenWidth >= 1201
-                    ? "extra"
-                    : ''
-                }
-              />
+               <img src={camera} alt="Camera" className={`${minimize} ${maximize}`}/> 
             </div>
             <div>
               <div className="name">Marry Doe</div>
