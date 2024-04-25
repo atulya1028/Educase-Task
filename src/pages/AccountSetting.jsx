99629899033303c5d078083265dfbf66be8c8b6c
atulya1028 <atulyaagrawal1996@gmail.com>
2024-04-25 13:12:58 +0530

    Responsive screen changes

--- src/pages/AccountSetting.jsx
+++ src/pages/AccountSetting.jsx
@@ -4,38 +4,63 @@ import profile from "../images/profile.png";
 import camera from "../images/camera.png";
 
 export const AccountSetting = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
 
-  const [screenWidth,setScreenWidth] = useState(window.screen);
+  const handleScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
+  const handleScreenHeight = () => {
+    setScreenHeight(window.innerHeight);
+  };
 
- const handleScreenWidth = ()=>{
-  setScreenWidth(window.innerWidth);
- }
-  
-  useEffect(()=>{
+  useEffect(() => {
     handleScreenWidth();
-    window.addEventListener('resize',handleScreenWidth);
+    window.addEventListener("resize", handleScreenWidth);
 
-    return ()=>{
-      window.removeEventListener('resize',handleScreenWidth);
-    }
-  },[]);
+    return () => {
+      window.removeEventListener("resize", handleScreenWidth);
+    };
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
+    handleScreenHeight();
+    window.addEventListener("resize", handleScreenHeight);
+
+    return () => {
+      window.removeEventListener("resize", handleScreenHeight);
+    };
+  }, []);
+
+  useEffect(() => {
     console.log(screenWidth);
-  })
+  });
 
-  console.log(screenWidth);
+  console.log("Width: ", screenWidth);
+  console.log("Height: ", window.innerHeight);
 
   return (
     <>
       <div className="header">Account Settings</div>
-      <div className="block">
+      <div className="block" style={{
+        backgroundColor:'bisque',
+        width: screenHeight == 1440 ? 340 :'auto'
+      }}>
         <div className="profile-main">
-          <div className="profile" >
-           <div className="intro">
-           <img src={profile} alt="Profile" />
-            <img src={camera} alt="Camera" className={screenWidth <=574 ? "camera" :screenWidth >=1440 ? 'xl-camera': 'full-camera'} />
-           </div>
+          <div className="profile">
+            <div className="intro" >
+              <img src={profile} alt="Profile" />
+              {screenHeight >=750 ? <img src={camera} alt="Camera" className="fl"/> :
+              screenWidth >= 1440 ? (
+                <img src={camera} alt="Camera" className="xl" />
+              ) : screenWidth >= 710 ? (
+                <img src={camera} alt="Camera" className="l" />
+              ) : screenWidth >= 576 ? (
+                <img src={camera} alt="Camera" className="m" />
+              ) : (
+                <img src={camera} alt="Camera" className="s" />
+              )}
+            </div>
             <div>
               <div className="name">Marry Doe</div>
               <div className="email">Marry@Gmail.Com</div>
